44e6a05a90092b4797e41b4af92bd09a775b2538
kenken <xux@live.ca>
2015-11-01 20:40:03 -0800

    updated q4

--- q4.cpp
+++ q4.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <fstream>
-#include <stdlib.h>
 #include <string.h>
-#include <sstream>
-#include <string>
-#include <math.h>
-#include <time.h>
-#include "sha1.h"
+#include <sstream> 
+#include "sha1.h"       // from https://code.google.com/p/smallsha1/
 
 using namespace std;
+
+const char oldPass[41] = "5E680C6D1A44D5C68A0175FEB6B750FCD18BE7DE";
 
 void getSha1(char* myPass, char* result) {
     unsigned char hash[20];
@@ -31,26 +29,33 @@ void toUpperCase(char* target) {
 
 
 int main() {
+    char myPass[100];
     char hashOut[41];
+    char offset[3];
+    string passStr, tmp;
+    stringstream offsetSS;
+    
     ofstream myfile ("37909090.program2.dif");
     
-    strcpy(tmp_MY_HASH, MY_HASH);
-    toUpperCase(tmp_MY_HASH);
+    cout << "Type new password: " << endl;
+    cin >> passStr;
     
-    for (int i=0; i<pow(strlen(LUT),strlen(passStr)); i++) {
-        //cout << passStr << endl;
-        getSha1(passStr ,hashOut);
-        incAll(passStr, passIndex);
-        
-        //if (i%10000000 == 0) {
-        //    cout << "10000000 passed" << endl;
-        //}
-        
-        if (myfile.is_open() && strcmp(hashOut, tmp_MY_HASH) == 0) {
-            myfile << passStr << endl << hashOut << endl;
-            cout << "Match found" << endl;
-            break;
-        }
+    strcpy(myPass, passStr.c_str());
+    
+    getSha1(myPass, hashOut);
+    toUpperCase(hashOut);
+    
+    myfile << "This difference file has been created by q4 executable" << endl << endl;
+    myfile << "37909090.program2.exe" << endl;
+    
+    for (int i=0; i<20; i++) {
+        offsetSS << hex << (i+212);
+        tmp = offsetSS.str();
+        offsetSS.str("");
+        strcpy(offset, tmp.c_str());
+        toUpperCase(offset);
+        myfile << "000127" << offset << ": " << oldPass[i*2] << oldPass[i*2+1];
+        myfile << " " << hashOut[i*2] << hashOut[i*2+1] << endl;
     }
 
     if (myfile.is_open())
